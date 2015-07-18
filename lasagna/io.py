23c353d182f2212c4bcd7f1f1455fbbb5547ee7e
feldman4 <feldman4@mit.edu>
2015-07-18 14:03:33 -0400

    fixed loading of channel colors and display ranges in ImageJ

--- lasagna/io.py
+++ lasagna/io.py
@@ -1,3 +1,4 @@
+import struct
 import numpy as np
 from skimage.external.tifffile import TiffFile, imsave, imread
 
@@ -12,26 +13,17 @@ UM_PER_PX = {'40X': 0.44,
 BINNING = 2
 OBJECTIVE = '40X'
 
-TAG_50839_BASE = (74, 73, 74, 73, 111, 102, 110, 105, 1, 0, 0, 0, 103, 110, 97, 114, 1, 0, 0,
-                  0, 115, 116, 117, 108, 4, 0, 0, 0, 83, 0, 111, 0, 102, 0, 116, 0, 119, 0, 97,
-                  0, 114, 0, 101, 0, 58, 0, 32, 0, 116, 0, 105, 0, 102, 0, 102, 0, 102, 0, 105,
-                  0, 108, 0, 101, 0, 46, 0, 112, 0, 121, 0, 10, 0, 68, 0, 97, 0, 116, 0, 101, 0,
-                  84, 0, 105, 0, 109, 0, 101, 0, 58, 0, 32, 0, 50, 0, 48, 0, 49, 0, 53, 0, 58, 0,
-                  48, 0, 55, 0, 58, 0, 48, 0, 57, 0, 32, 0, 49, 0, 53, 0, 58, 0, 52, 0, 49, 0, 58,
-                  0, 48, 0, 52, 0, 10, 0, 0, 0, 0, 0, 0, 144, 120, 64, 0, 0, 0, 0, 236, 168, 192,
-                  64, 0, 0, 0, 0, 0, 224, 135, 64, 0, 0, 0, 0, 128, 169, 226, 64, 0, 0, 0, 0, 224,
-                  108, 141, 64, 0, 0, 0, 0, 224, 168, 150, 64, 0, 0, 0, 0, 0, 224, 135, 64, 0, 0, 0,
-                  0, 128, 169, 226, 64)
-
 RED = tuple(range(256) + [0] * 512)
-GREEN = tuple([0]*256 + range(256) + [0]*256)
+GREEN = tuple([0] * 256 + range(256) + [0] * 256)
 BLUE = tuple([0] * 512 + range(256))
-MAGENTA = tuple(range(256) + [0] * 512 + range(256))
+MAGENTA = tuple(range(256) + [0] * 256 + range(256))
 
 DEFAULT_LUTS = (BLUE, GREEN, RED, MAGENTA)
 
+# http://rsb.info.nih.gov/ij/developer/source/ij/io/TiffEncoder.java.html
 
-def save_hyperstack(name, data, autocast=True, resolution=None, luts=None):
+def save_hyperstack(name, data, autocast=True, resolution=None,
+                    luts=None, display_ranges=None, compress=0):
     """input ND array dimensions as ([time], [z slice], channel, y, x)
     leading dimensions beyond 5 could be wrapped into time, not implemented
     """
@@ -39,24 +31,35 @@ def save_hyperstack(name, data, autocast=True, resolution=None, luts=None):
     if resolution is None:
         resolution = (1. / (UM_PER_PX[OBJECTIVE] * BINNING),) * 2
     if luts is None:
-        luts = [x for x,_ in zip(DEFAULT_LUTS, range(nchannels))]
+        luts = [x for x, _ in zip(DEFAULT_LUTS, range(nchannels))]
+    if display_ranges is None:
+        display_ranges = tuple([(x.min(), x.max())
+                                for x in np.rollaxis(data, -3)])
 
-    # save as uint16
+    # convert to uint16
     tmp = data.copy()
     if autocast:
         tmp = tmp.astype(np.uint16)
 
+    # metadata encoding LUTs and display ranges
     description = ij_description(data.shape)
     tag_50838 = ij_tag_50838(nchannels)
-    tag_50839 = ij_tag_50839(luts)
+    tag_50839 = ij_tag_50839(luts, display_ranges)
 
     imsave(name, tmp, photometric='minisblack',
-           description=description, resolution=resolution,
+           description=description, resolution=resolution, compress=compress,
            extratags=[(50838, 'I', len(tag_50838), tag_50838, True),
                       (50839, 'B', len(tag_50839), tag_50839, True),
                       ])
 
+    return description, tag_50838, tag_50839
+
+
 def ij_description(shape):
+    """Format ImageJ description for hyperstack.
+    :param shape:
+    :return:
+    """
     s = shape[:-2]
     if not s:
         return imagej_description % (1, 1, 1, 1)
@@ -72,14 +75,27 @@ def ij_description(shape):
 
 
 def ij_tag_50838(nchannels):
-    px_size = (16 * nchannels,)
-    luts = (256*3,)*nchannels
-    return (28, 104) + px_size + luts
-
-
-def ij_tag_50839(luts):
-    """Return tag 50839 for imagej header (info, range, luts)
-    :param luts: iterable containing 255*3 ints in range 0-255 specifying RGB, io.RED etc available
+    """ImageJ uses tag 50838 to indicate size of metadata elements (e.g., 768 bytes per ROI)
+    :param nchannels:
     :return:
     """
-    return TAG_50839_BASE + sum([list(x) for x in luts],[])
+    info_block = (20,)  # summary of metadata fields
+    display_block = (16 * nchannels,)  # display range block
+    luts_block = (256 * 3,) * nchannels  #
+    return info_block + display_block + luts_block
+
+
+def ij_tag_50839(luts, display_ranges):
+    """ImageJ uses tag 50839 to store metadata. Only range and luts are implemented here.
+    :param tuple luts: tuple of 255*3=768 8-bit ints specifying RGB. Class constants io.RED etc may be used.
+    :param tuple display_ranges: tuple of (min, max) pairs for
+    :return:
+    """
+    d = struct.pack('<' + 'd' * len(display_ranges) * 2, *[y for x in display_ranges for y in x])
+    # insert display ranges
+    tag = ''.join(['JIJI',
+                   'gnar\x01\x00\x00\x00',
+                   'stul%s\x00\x00\x00' % chr(len(luts)),
+                   d])
+    tag = struct.unpack('<' + 'B' * len(tag), tag)
+    return tag + tuple(sum([list(x) for x in luts], []))
