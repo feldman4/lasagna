4a726a00c69b84a2f0fbe1a3cdab7d66cacfe913
feldman4 <feldman4@mit.edu>
2015-08-24 17:52:15 -0400

    updated 20150817

--- lasagna/config.py
+++ lasagna/config.py
@@ -3,6 +3,7 @@ import os
 up = os.path.dirname
 home = up(up(__file__))
 paths = None
+calibration = None
 visitor_font = '/broad/blainey_lab/David/packages/lasagna/resources/fonts/visitor1.ttf'
 fonts = os.path.join(home, 'resources', 'fonts')
 luts = os.path.join(home, 'resources', 'luts')
--- lasagna/pipelines/_20150817.py
+++ lasagna/pipelines/_20150817.py
@@ -1,4 +1,35 @@
+import lasagna.io
+import lasagna.config
+import lasagna.process
+
 display_ranges = ((500, 20000),
                   (500, 3500),
                   (500, 3500),
                   (500, 3500))
+
+dataset = '20150817 6 round'
+
+
+def setup():
+    lasagna.config.paths = lasagna.io.Paths(dataset)
+    lasagna.config.paths.add_analysis('raw', 'calibrated')
+
+    config_path = lasagna.config.paths.full(lasagna.config.paths.calibrations[0])
+    lasagna.config.calibration = (lasagna.process.Calibration(config_path,
+                                                              dead_pixels_file='dead_pixels_empirical.tif'))
+
+
+
+def calibrate(row, paths, calibration):
+    import numpy as np
+    import lasagna.io
+
+    channels = ['Cy3', 'Cy3', 'A594', 'Atto647']
+    luts = [lasagna.io.BLUE, lasagna.io.GREEN, lasagna.io.RED, lasagna.io.MAGENTA]
+    raw, calibrated = row['raw'], row['calibrated']
+    raw_data = lasagna.io.read_stack(paths.full(raw))
+    raw_data = np.array([calibration.fix_dead_pixels(frame) for frame in raw_data])
+    fixed_data = np.array([calibration.fix_illumination(frame, channel=channel)
+                           for frame, channel in zip(raw_data, channels)])
+    lasagna.io.save_hyperstack(paths.full(calibrated), fixed_data, luts=luts)
+
