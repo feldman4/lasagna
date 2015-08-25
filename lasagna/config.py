d56415b27cbe9a0a2d9d2d48d08f95d819234336
feldman4 <feldman4@mit.edu>
2015-08-25 10:43:34 -0400

    initialization for sge

--- lasagna/config.py
+++ lasagna/config.py
@@ -4,6 +4,7 @@ up = os.path.dirname
 home = up(up(__file__))
 paths = None
 calibration = None
+calibration_short = None
 visitor_font = '/broad/blainey_lab/David/packages/lasagna/resources/fonts/visitor1.ttf'
 fonts = os.path.join(home, 'resources', 'fonts')
 luts = os.path.join(home, 'resources', 'luts')
--- lasagna/pipelines/_20150817.py
+++ lasagna/pipelines/_20150817.py
@@ -1,6 +1,7 @@
 import lasagna.io
 import lasagna.config
 import lasagna.process
+import copy
 
 display_ranges = ((500, 20000),
                   (500, 3500),
@@ -17,7 +18,9 @@ def setup():
     config_path = lasagna.config.paths.full(lasagna.config.paths.calibrations[0])
     lasagna.config.calibration = (lasagna.process.Calibration(config_path,
                                                               dead_pixels_file='dead_pixels_empirical.tif'))
-
+    c = copy.deepcopy(lasagna.config.calibration)
+    c.calibration = None
+    lasagna.config.calibration_short = c
 
 
 def calibrate(row, paths, calibration):
@@ -33,3 +36,61 @@ def calibrate(row, paths, calibration):
                            for frame, channel in zip(raw_data, channels)])
     lasagna.io.save_hyperstack(paths.full(calibrated), fixed_data, luts=luts)
 
+
+def calibrate_sge(dfs, lview):
+    """Apply current calibration, with results saved according to current paths. Farm out jobs to
+     provided load-balanced view.
+    :param dfs:
+    :param lview:
+    :return:
+    """
+    async_results = []
+    c_ = copy.deepcopy(lasagna.config.calibration)
+    c_.calibration = None
+
+    for df in dfs:
+        def func(df_, paths=lasagna.config.paths, calibration=c_):
+            import lasagna.pipelines._20150817 as pipeline
+            for ix, row in df_.iterrows():
+                pipeline.calibrate(row, paths, calibration)
+        async_results += [lview.apply_async(func, df)]
+
+    return async_results
+
+
+def initialize_engines(client):
+    dview = client[:]
+    dview.execute('import numpy as np')
+    dview.execute('import lasagna.io')
+    dview.execute('import lasagna.process')
+    dview.execute('import lasagna.pipelines._20150817')
+
+    dview['paths'] = lasagna.config.paths
+    dview['calibration'] = lasagna.config.calibration_short
+
+
+
+def my_func(df, y, paths):
+    import numpy as np
+    import lasagna.io, lasagna.process
+    stitch = df.ix[0, 'calibrated'].replace('analysis/calibrated/raw',
+                                            'stitch')
+    stitch = paths.full(stitch)
+    if not os.path.isdir(os.path.dirname(stitch)):
+        os.makedirs(os.path.dirname(stitch))
+        print 'made dir', os.path.dirname(stitch)
+
+    data = np.array([lasagna.io.read_stack(paths.full(f))
+                  for f in df['raw']])
+    grid = np.sqrt(data.size / (4*1024*1024))
+    data = data.reshape([grid,grid,4,1024,1024])
+
+#     off = lasagna.process.stitch_grid(data[:,:,0,...], 925./1024)
+#     y = lasagna.process.compress_offsets(off)
+
+
+    data2 = np.array([lasagna.process.alpha_blend(data[:,:,i,:,:], y)
+                      for i in range(data.shape[2])])
+
+    lasagna.io.save_hyperstack(paths.full(stitch), data2)
+    return 'saved', stitch
