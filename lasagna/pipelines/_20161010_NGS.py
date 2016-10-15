be6d35273f6e2b9892119bacf28d3cba0efc247a
feldman <feldman4@mit.edu>
2016-10-14 23:15:50 -0400

    work

--- lasagna/pipelines/_20161010_NGS.py
+++ lasagna/pipelines/_20161010_NGS.py
@@ -8,6 +8,14 @@ import numpy as np
 # sns.set(style='white', font_scale=1.5)
 
 
+sgRNAs = {'sgV-101': 'GAGACCAGCAGAACCGACAA',
+ 'sgV-106': 'CGGGAGGGCAGGGCACGAAA',
+ 'sgV-107': 'GCGCAACAGAGAGGGGAGCG',
+ 'sgV-116': 'AGGGCACAGACCCCAGGGAG',
+ 'sgV-117': 'CAGGGGGGAGCGAAAGAGAA'}
+
+
+
 def read_fastq(filepath):
     with open(filepath, 'r') as fh:
         return fh.read().split('\n')[1::4]
@@ -25,7 +33,7 @@ def make_sample_table(prefix=''):
     get_well = lambda f: re.findall('fastq/(...)', f)[0]
     df['well'] = [get_well(f) for f in df['R1']]
     df['reads'] = [len(read_fastq(f)) for f in df['R1']]
-    df = df[['sample', 'reads', 'R1', 'R2']]
+    df = df[['well', 'reads', 'R1', 'R2']]
 
     df['row'] = [s[0] for s in df['well']]
     df['col'] = [s[1:] for s in df['well']]
@@ -93,10 +101,10 @@ def add_features(df, add_well_info_from_sample=True):
         df['run'] = [s[0] for s in df['sample']]
         df['well'] = [s[1] for s in df['sample']]
         df['row'] = [s[0] for s in df['well']]
-        df['col'] = [s[1:] for s in df['well']]
+        df['col'] = [int(s[1:]) for s in df['well']]
 
     df['length'] = [len(s) for s in df['match']]
-    df['frame'] = df['length'] % 3
+    df['frame'] = (df['length'] + 2) % 3
     df['kozak'] = ['GCCACCATG' in s for s in df['match']]
     ORFs = [re.findall('(?=ATG(.*))', m) for m in df['match']]
     df['ATG_0'] = [any((len(x)%3 == 0) for x in hit) for hit in ORFs]
@@ -115,11 +123,24 @@ def add_features(df, add_well_info_from_sample=True):
     return df.reset_index(drop=True)
 
 
-def load_conditions(sheet):
+def mark_controls(df, target_info):
+    df = df.copy()
+    # should there be signal?
+    A = (df['dox'] == 'no dox')
+    B = (df['sgRNA'] != target_info.loc[df['target']]['sgRNA'])
+    C = df['sgRNA'].apply(lambda s: 'K' in s)
+
+    df['control'] = A | (B & ~C)
+    df['library'] = ['K' in s for s in df['sgRNA']]
+
+    return df
+
+
+def load_conditions(sheet, g_file='Lasagna NGS'):
     """ Load conditions from Lasagna NGS.
     """
     from lasagna.conditions_ import Experiment
-    exp = Experiment(sheet, g_file='Lasagna NGS')
+    exp = Experiment(sheet, g_file=g_file)
     ivt = exp.ind_vars_table
     ivt['well'] = [s[0] + '0' + s[1] if len(s)==2 else s for s in ivt.index]
     ivt['run'] = sheet
@@ -157,7 +178,7 @@ def display_ind_var(df, ind_var, run=None, fix_rows_cols=True):
 def load_target_info():
     from lasagna.conditions_ import load_sheet
     x = load_sheet('targets', g_file='inventory for a time machine')
-    return pd.DataFrame(x[1:], columns=x[0])
+    return pd.DataFrame(x[1:], columns=x[0]).set_index('target')
 
 
 def msa(template, sequences, match=2, mismatch=-1, open=-1, extend=-0.5):
@@ -216,9 +237,9 @@ def display_target_sgRNAs(target, df, target_info):
 
     collapsed_sgRNAs = collapse_sgRNAs(df)
     
-    sgRNA = target_info.set_index('target').loc[target, 'sgRNA']
-    t = target_info.set_index('target').loc[target, 'FWD_seq']
-    s = target_info.set_index('target').loc[target, 'sgRNA_seq']
+    sgRNA = target_info.loc[target, 'sgRNA']
+    t = target_info.loc[target, 'FWD_seq']
+    s = target_info.loc[target, 'sgRNA_seq']
 
     sequences, counts = collapsed_sgRNAs.loc[sgRNA].as_matrix().T
     aligned = msa(t, sequences)
@@ -239,23 +260,176 @@ def display_target_sgRNAs(target, df, target_info):
     
     return df2
 
-def normalize_sgRNA_counts(table, df):
-    # 
-    cols = slice('sgV-101', 'sgV-3K-5')
-    df = df.copy()
-    table = table.copy()
-    x = df.query('day=="d9"&pattern_name=="sgRNA"&dox=="dox"&row!="A"')
-    y = x[['sgRNA', 'sample', 'mapped']].drop_duplicates(['sgRNA', 'sample'])
-    y = y.groupby('sgRNA').sum()
-    
-    a = table.loc[:, cols].T.copy()
-    b = a.divide(y['mapped'], axis=0).T
-    table.loc[:, cols] = b
-    return table
-
-
 def count_sgRNAs_d9(df):
     """Used for normalizing 
     """
     x = df.query('day=="d9"&pattern_name=="sgRNA"&dox=="dox"&row!="A"')
     return x.pivot_table(values='fraction', columns='sgRNA', index='match')
+
+
+
+oddballs = {
+    '101.fcs' : 'E01',
+    '102.fcs' : 'E02',
+    '106.fcs' : 'E03',
+    '107.fcs' : 'E04',
+    '116.fcs' : 'E05',
+    '117.fcs' : 'E06',
+
+    '01-96W-101.fcs' : 'E01',
+    '01-96W-102.fcs' : 'E02',
+    '01-96W-106.fcs' : 'E03',
+    '01-96W-107.fcs' : 'E04',
+    '01-96W-116.fcs' : 'E05',
+    '01-96W-117.fcs' : 'E06',
+
+    'sgV101.fcs' : 'E01',
+    'sgV102.fcs' : 'E02',
+    'sgV106.fcs' : 'E03',
+    'sgV107.fcs' : 'E04',
+    'sgV116.fcs' : 'E05',
+    'sgV117.fcs' : 'E06',
+
+    'A1-.fcs' : 'C01',
+    'A2-.fcs' : 'C02',
+    'A3-.fcs' : 'C03',
+    'A4-.fcs' : 'C04',
+    'A5-.fcs' : 'C05',
+    'A6-.fcs' : 'C06',
+    'B1-.fcs' : 'D01',
+    'B2-.fcs' : 'D02',
+    'B3-.fcs' : 'D03',
+    'B4-.fcs' : 'D04',
+    'B5-.fcs' : 'D05',
+    'B6-.fcs' : 'D06',
+}
+oddballs_d7 = {
+
+    '01-96W-A1.fcs' : 'C01',
+    '01-96W-A2.fcs' : 'C02',
+    '01-96W-A3.fcs' : 'C03',
+    '01-96W-A4.fcs' : 'C04',
+    '01-96W-A5.fcs' : 'C05',
+    '01-96W-A6.fcs' : 'C06',
+    '01-96W-B1.fcs' : 'D01',
+    '01-96W-B2.fcs' : 'D02',
+    '01-96W-B3.fcs' : 'D03',
+    '01-96W-B4.fcs' : 'D04',
+    '01-96W-B5.fcs' : 'D05',
+    '01-96W-B6.fcs' : 'D06',
+   }
+
+
+def load_fcs_stats(csv):
+    columns = {
+    'cells | Count': 'cells',
+    'cells/Single Cells | Count': 'single cells',
+    'Cells_2 | Count': 'cells',
+    'Cells_2/Single Cells | Count': 'single cells',
+    'cells/Single Cells/GFP+_d3_cytoflex | Count': 'GFP+',
+    'cells/Single Cells/GFP+_d5_cytoflex | Count': 'GFP+',
+    'cells/Single Cells/GFP+_d7_cytoflex | Count': 'GFP+',
+    'cells/Single Cells/GFP+_d9_cytoflex | Count': 'GFP+',
+    'Cells_2/Single Cells/GFP+ | Count': 'GFP+',
+    'cells/Single Cells/MC+ | Count': 'mCherry+',
+    'cells/Single Cells/MC+ | Count': 'mCherry+',
+    'Cells_2/Single Cells/MC+ | Count': 'mCherry+',
+    'Unnamed: 0': 'fcs'}
+    
+    df = pd.read_csv(csv)
+    columns = {k: columns[k] for k in columns if k in df.columns}
+    df = df.rename(columns=columns).loc[:,columns.values()]
+    df = df[~df['fcs'].isin(['Mean', 'SD'])]
+
+    wells = []
+    for f in df['fcs']:
+        sony_names = '3K', '1K', 'Data Source'
+        if any(n in f for n in sony_names):
+            wells += ['tube']
+            continue
+        if f in oddballs:
+            wells += [oddballs[f]]
+            continue
+
+        if 'd7' in csv and f in oddballs_d7:
+            wells += [oddballs_d7[f]]
+            continue
+
+        well_pattern = '([A-H][0-1][1-9])|([A-H][1-9])'
+        matches = re.findall(well_pattern, f)
+        if matches:
+            fmt0, fmt1 = matches[0]
+            if fmt1:
+                well = fmt1[0] + '%02d' % int(fmt1[1:])
+            else:
+                well = fmt0
+        else:
+            raise ValueError('unrecognized well in %s' % f)
+
+        wells += [well]
+
+    df['well'] = wells
+
+    df['row']  = [w[0] if w is not 'tube' else None for w in df['well']]
+    df['col']  = [int(w[1:]) if w is not 'tube' else None for w in df['well']]
+
+    df['negative'] = (df['single cells'] 
+                      - df['mCherry+'] 
+                      - df['GFP+'])
+
+    df['% negative'] = (df['negative'] / df['single cells']) * 100
+    df['% mCherry+'] = (df['mCherry+'] / df['single cells']) * 100
+    df['% GFP+']     = (df['GFP+'] / df['single cells']) * 100
+
+    return df
+
+
+def load_flowstats():
+    arr = []
+    for csv in glob('FlowStats/*.csv'):
+        run = csv.split('/')[1]
+        day = run[:2]
+        df = load_fcs_stats(csv)
+        df['run'] = run
+        df['day'] = day
+
+        df['instrument'] = None
+        if 'cytoflex' in csv.lower():
+            df['instrument'] = 'cytoflex'
+        if 'sony' in csv.lower():
+            df['instrument'] = 'sony'
+
+        arr += [df]
+    return pd.concat(arr)
+
+
+
+
+
+
+def load_target_info():
+    from lasagna.conditions_ import load_sheet
+    x = load_sheet('targets', g_file='inventory for a time machine')
+    df = pd.DataFrame(x[1:], columns=x[0])
+    return df.set_index('target') 
+
+
+
+def add_closest_sgRNA(df, sgRNAs):
+
+    def closest_sgRNAs(sequence, sgRNAs):
+        from Levenshtein import distance
+        return sorted([[distance(sequence, s), name] for name, s in sgRNAs.items()])
+
+    df = df.copy()
+    
+    df['closest_sgRNA'] = 'other'
+    df['closest_sgRNA_dist'] = -1
+    
+    filt = df['pattern_name'] == 'sgRNA'
+    closest = [closest_sgRNAs(s, sgRNAs) for s in df.loc[filt, 'match']]
+    
+    df.loc[filt, 'closest_sgRNA'] = [n[0][1] for n in closest]
+    df.loc[filt, 'closest_sgRNA_dist'] = [n[0][0] for n in closest]
+    
+    return df
