3888cde48ff5bd64e08efcb6b2653076ac06ba41
feldman4 <feldman4@users.noreply.github.com>
2018-06-21 11:23:24 -0400

    in situ

--- lasagna/in_situ.py
+++ lasagna/in_situ.py
@@ -2,31 +2,8 @@
 import numpy as np
 import pandas as pd
 import functools
-
-# df_reads <=> fastq
-
-# fields
-
-WELL = 'well'
-TILE = 'tile'
-CELL = 'cell'
-BLOB = 'blob'
-CHANNEL = 'channel'
-CYCLE = 'cycle'
-INTENSITY = 'intensity'
-CYCLES_IN_SITU = 'cycles_in_situ'
-BARCODE = 'barcode'
-BARCODE_COUNT = 'barcode_count'
-BARCODE_0 = 'cell_barcode_0'
-BARCODE_1 = 'cell_barcode_1'
-BARCODE_COUNT_0 = 'cell_barcode_count_0'
-BARCODE_COUNT_1 = 'cell_barcode_count_1'
-GLOBAL_X = 'global_x'
-GLOBAL_Y = 'global_y'
-CLUSTER = 'cluster'
-SUBPOOL = 'subpool'
-SGRNA_NAME = 'sgRNA_name'
-SGRNA_DESIGN = 'sgRNA_design'
+from lasagna.schema import *
+from natsort import natsorted
 
 IMAGING_ORDER = 'GTAC'
 
